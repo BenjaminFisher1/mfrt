9498af489491506de98eaf79aec386a78fd95fdf
Ben Fisher <BenLfish00@gmail.com>
2025-12-29 16:11:10 -0500

    create mfrt package
    Move cli.py to mfrt package, change name in setup.py, edit __init__.py

---
+++ mfrt/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package initializer for mfrt.
+"""
+
+__all__ = ["cli"]
---
+++ mfrt/cli.py
@@ -0,0 +1,30 @@
+import click
+import os
+
+@click.command()
+@click.option('--path', default='.', prompt='Path to directory (leave blank for current working directory)', help='Path to directory containing files to rename')
+@click.option('--nameconvention', default='file', prompt='Naming convention', help='Naming convention for renamed files')
+def main(path, nameconvention):
+    """Lightweight Program to rename files in bulk based on a specified naming convention."""
+    click.echo(f'Renaming files in {path} using naming convention: {nameconvention}')
+
+
+    #Create list of FILES in directory, ignoring subdirectories
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+    files.sort()  
+
+    for i, file in enumerate(files, 1):
+        old_name = os.path.join(path, file)
+
+        #get extension
+        _, extension = os.path.splitext(file)
+
+        #Create new name with extension
+        new_name = f"{nameconvention}_{i}{extension}"
+        new_path = os.path.join(path, new_name)
+
+        click.echo(f'Renaming "{file}" to "{new_name}"')
+        os.rename(old_name, new_name)
+
+    click.echo('Renaming complete.')
